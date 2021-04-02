33d40f81ddbaef9bdc29d1924f403621d2cb706f
Rouven <rouven.zeus@gmx.net>
2021-04-02 12:47:03 +0200

    Add Imprint.
    Added empty imprint page. Content needs to be put.

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,6 @@
+var currentYear = new Date().getFullYear();
+document.getElementById("changeDate").textContent = "\u00A9" + currentYear;
+
 try {
     //not possible at main page, as discussion is not defined there
     updateDiscussion();
--- scripts/nav.js
+++ scripts/nav.js
@@ -1,15 +1,15 @@
 function toggleTopMenu() {
-    var navLinkSpan = document.getElementById("navLinks");
+    var navLinkSpan = document.getElementById("navTopLinks");
     var navLinkNumber = navLinkSpan.querySelectorAll("a").length;
-    var navDiv = document.getElementById("nav");
+    var navDiv = document.getElementById("navTop");
     var classList = navLinkSpan.classList;
     var menuHeight = navLinkNumber * 40 + 65;
-    if (classList.toggle("navLinksTop")) {
-        navDiv.style.height = "6vh";
+    if (classList.toggle("navTopLinksAbove")) {
+        navDiv.style.height = "var(--navTopHeight)";
     } else {
         navDiv.style.height = menuHeight + "px";
     }
-    classList.toggle("navLinksBelow");
+    classList.toggle("navTopLinksBelow");
 }
 
 function addMenuButtonListener() {
